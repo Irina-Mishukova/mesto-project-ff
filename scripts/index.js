158f42df57838f7dbe8df1f4e1a88492b16dd101
Irina Mishukova <snysnyp@gmail.com>
2024-03-11 22:10:02 +0700

    remove_button_dev

--- scripts/index.js
+++ scripts/index.js
@@ -10,22 +10,51 @@
 
 
 
-initialCards.forEach(function(item){
-    console.log(item);
-});
+// initialCards.forEach(function(item){
+//     console.log(item);
+// });
 
 
 const placesList = document.querySelector('.places__list');
 // console.log(placesList);
 
-const placesListTemplate = document.querySelector('#card-template').content;
+const cardTemplate = document.querySelector('#card-template').content;
 
 // console.log(placesListTemplate);
 
+
+
 initialCards.forEach(function(element){
-    const initialCardsElement = placesListTemplate.cloneNode(true);
+    const initialCardsElement = cardTemplate.cloneNode(true);
     initialCardsElement.querySelector('.card__title').textContent = element.name;
     initialCardsElement.querySelector('.card__image').src = element.link;
 
     placesList.append(initialCardsElement)
 });
+
+
+// function renderDeleteCard() {
+
+// };
+
+// cardDeleteButton.addEventListener('click', function() {
+//     const card = document.querySelector('.places__item card');
+//     card.remove();
+// });
+
+
+
+    
+const cardDeleteButton = document.querySelector('.card__delete-button');
+
+const listItem = document.querySelector('.places__item')
+
+function deleteCard() {
+    console.log('push the button');
+    listItem.remove();
+}
+
+// deleteCard();
+
+cardDeleteButton.addEventListener('click', deleteCard);
+
