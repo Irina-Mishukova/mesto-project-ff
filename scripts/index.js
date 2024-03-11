8805d02ec64f156204c3d2dbfae4b5681d3696e4
Irina Mishukova <snysnyp@gmail.com>
2024-03-11 15:47:07 +0700

    add_cards

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,25 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+
+
+initialCards.forEach(function(item){
+    console.log(item);
+});
+
+
+const placesList = document.querySelector('.places__list');
+// console.log(placesList);
+
+const placesListTemplate = document.querySelector('#card-template').content;
+
+// console.log(placesListTemplate);
+
+initialCards.forEach(function(element){
+    const initialCardsElement = placesListTemplate.cloneNode(true);
+    initialCardsElement.querySelector('.card__title').textContent = element.name;
+    initialCardsElement.querySelector('.card__image').href = element.link;
+
+    placesList.append(initialCardsElement)
+});
