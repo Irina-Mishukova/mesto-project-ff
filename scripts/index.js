eae1c52d92d3cb3bad090adbb82321bcbb2a2405
Irina Mishukova <snysnyp@gmail.com>
2024-03-14 14:52:19 +0700

    comment_fix_cl

--- scripts/index.js
+++ scripts/index.js
@@ -24,6 +24,7 @@ const cardDeleteButton = cards.querySelector('.card__delete-button');
 cardDeleteButton.addEventListener('click', function(evt){
     deleteCard(evt) ;
 });
+
 return cards;
 };
 
@@ -36,35 +37,3 @@ function deleteCard(evt) {
 evt.target.closest('.places__item').remove();
 }
 
-
-
-
-
-
-
-
-
-
-
-
-// const placesList = document.querySelector('.places__list');
-
-// const cardTemplate = document.querySelector('#card-template').content;
-
-// initialCards.forEach(function(element){
-// const cards = cardTemplate.cloneNode(true);
-// cards.querySelector('.card__title').textContent = element.name;
-// cards.querySelector('.card__image').src = element.link;
-
-// const cardDeleteButton = cards.querySelector('.card__delete-button');
-
-// cardDeleteButton.addEventListener('click', function(evt){
-//     deleteCard(evt) ;
-// });
-
-// placesList.append(cards);
-// });
-
-// function deleteCard(evt) {
-// evt.target.closest('.places__item').remove();
-// }
