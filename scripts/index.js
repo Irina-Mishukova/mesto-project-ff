54a7a11ac948baa9325d5430f15fb6b0c8ace10d
Irina Mishukova <snysnyp@gmail.com>
2024-03-14 12:16:55 +0700

    comment_fix

--- scripts/index.js
+++ scripts/index.js
@@ -9,24 +9,62 @@
 // @todo: Вывести карточки на страницу
 
 
-    const placesList = document.querySelector('.places__list');
+const placesList = document.querySelector('.places__list');
 
-    const cardTemplate = document.querySelector('#card-template').content;
+const cardTemplate = document.querySelector('#card-template').content;
 
-initialCards.forEach(function(element){
-    const cards = cardTemplate.cloneNode(true);
-    cards.querySelector('.card__title').textContent = element.name;
-    cards.querySelector('.card__image').src = element.link;
-    
-    const cardDeleteButton = cards.querySelector('.card__delete-button');
+function createCard(element, {deleteCard}) {
+const cards = cardTemplate.cloneNode(true);
+cards.querySelector('.card__title').textContent = element.name;
+cards.querySelector('.card__image').src = element.link;
+cards.querySelector('.card__image').alt = element.name;
 
-    cardDeleteButton.addEventListener('click', function(evt){
-        deleteCard(evt) ;
+const cardDeleteButton = cards.querySelector('.card__delete-button');
+
+cardDeleteButton.addEventListener('click', function(evt){
+    deleteCard(evt) ;
+});
+return cards;
+};
+
+initialCards.forEach(function (element) {
+    const cards = createCard(element, { deleteCard });
+    placesList.append(cards);
     });
 
-    placesList.append(cards);
-});
-
 function deleteCard(evt) {
-   evt.target.closest('.places__item').remove();
+evt.target.closest('.places__item').remove();
 }
+
+
+
+
+
+
+
+
+
+
+
+
+// const placesList = document.querySelector('.places__list');
+
+// const cardTemplate = document.querySelector('#card-template').content;
+
+// initialCards.forEach(function(element){
+// const cards = cardTemplate.cloneNode(true);
+// cards.querySelector('.card__title').textContent = element.name;
+// cards.querySelector('.card__image').src = element.link;
+
+// const cardDeleteButton = cards.querySelector('.card__delete-button');
+
+// cardDeleteButton.addEventListener('click', function(evt){
+//     deleteCard(evt) ;
+// });
+
+// placesList.append(cards);
+// });
+
+// function deleteCard(evt) {
+// evt.target.closest('.places__item').remove();
+// }
